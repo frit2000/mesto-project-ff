c4542c81910ebcce692d74117f6d69bac61b67ba
frit2000 <frit2000@yandex.ru>
2024-09-16 17:53:44 +0300

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placeList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function addCard(cardName, cardLink, delCard) {
+  const placeItem = cardTemplate.querySelector(".places__item").cloneNode(true);
+  placeItem.querySelector(".card__title").textContent = cardName;
+  placeItem.querySelector(".card__image").src = cardLink;
+  placeItem.querySelector(".card__image").alt = cardName;
+
+  const delButton = placeItem.querySelector(".card__delete-button");
+  delButton.addEventListener("click", delCard);
+
+  return placeItem;
+}
 
 // @todo: Функция удаления карточки
-
+function delCard(evt) {
+  const itemToDel = evt.target.closest(".places__item");
+  itemToDel.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+  placeList.append(addCard(element.name, element.link, delCard));
+});
